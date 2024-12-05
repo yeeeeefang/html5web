c2e523c2e9328a6e69431140615c84cac7a79b43
yeeeeefang <sweetcat881314@gmail.com>
2024-12-05 16:12:02 +0800

    1205輪播圖片

---
+++ web/js/carousel.js
@@ -0,0 +1,12 @@
+function bannerSwitcher() {
+    next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
+    if (next.length) next.prop('checked', true);
+    else $('.sec-1-input').first().prop('checked', true);
+  }
+
+  var bannerTimer = setInterval(bannerSwitcher, 5000);
+
+  $('nav .controls label').click(function() {
+    clearInterval(bannerTimer);
+    bannerTimer = setInterval(bannerSwitcher, 5000)
+  });
